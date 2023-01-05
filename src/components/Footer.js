b102041b06acbba0e03b0892e90d2a0d96ffcd91
Your Name <you@example.com>
2023-01-05 09:46:04 -0500

    changes on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import "../styles/Footer.css"
 function Footer() {
   return (
     <div className = "footer">
-        <div className= "socialMedia">
+        <div className= "socialMedia"> 
             <LinkedInIcon/>
             <GitHubIcon/>
 
