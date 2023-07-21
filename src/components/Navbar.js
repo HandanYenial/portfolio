7cb5cfd128f5a867aa37cdc596f9c7de19d849ae
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2023-07-20 23:03:49 -0400

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,25 @@
-import React , { useState, useEffect } from 'react'
-import { Link, useLocation } from "react-router-dom"; //to create links in the navbar //useLocation hook 
+import React , { useState, useEffect } from 'react';
+import { Link, useLocation } from "react-router-dom";    //to create links in the navbar //useLocation hook 
 import "../styles/Navbar.css"
-import ReorderOutlinedIcon from '@mui/icons-material/ReorderOutlined';; //hmburger button for navbar
+import ReorderOutlinedIcon from '@mui/icons-material/ReorderOutlined';;     //hamburger button for navbar
 
 function Navbar() {
-    //create a state when t expand the navbar
-    const [expandNavbar , setExpandNavbar] = useState(false); //will be false initially
-    //will get infromation where in the website user in
+    //create a state when expand the navbar
+    const [expandNavbar , setExpandNavbar] = useState(false);     //will be false initially
+    //will get information where in the website user in
     const location = useLocation();
-    // a function which will run when the location changes
+    // a function that will run when the location changes
     useEffect(() => {
         setExpandNavbar(false);
     }, [location] )
 
+    
+    //If expandNavbar is true set it to open, if not set it to close.
   return (
-    // if expandNavbar is true set it to open, if not set it to close.
+   
     <div className = "navbar" id={expandNavbar ? "open" : "close"}> 
     <div className = "toggleButton">
-        {/* so whenever clcik on the button, the state will change to the opposite of the previous state
+        {/* so whenever click on the button, the state will change to the opposite of the previous state
         if it is false--> true, if it is true it will become false. */}
         <button onClick = { 
             () => {
